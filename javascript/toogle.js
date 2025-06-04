5db5e120a582c3b2f94bb5f2fed050c32b820bc9
ratatouilleee <gabriellelintong01@gmail.com>
2025-06-04 17:38:16 +0700

    Add files via upload

---
+++ javascript/toogle.js
@@ -0,0 +1,9 @@
+<script>
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+</script>
